e833ec2d8fe3f4a62494eda61517f54f37353ad1
Ruchi Kumari <122676573+Ruchi-here@users.noreply.github.com>
2026-03-23 01:37:35 +0530

    Update Reward.java

--- StickHeroGame/src/main/java/com/example/stickherog/Reward.java
+++ StickHeroGame/src/main/java/com/example/stickherog/Reward.java
@@ -3,38 +3,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-//class Reward {
-//    private double x;
-//    private double y;
-//
-//    public void setX(double x) {
-//        this.x = x;
-//    }
-//
-//    public void setY(double y) {
-//        this.y = y;
-//    }
-//
-//    public Reward(double x, double y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//
-//    public void render(GraphicsContext gc, double cameraOffset) {
-//        // Implement rendering logic for the reward
-//        gc.setFill(Color.RED);
-//        gc.fillOval(x-cameraOffset, y, 10, 10);  // Example rendering; customize as needed
-//    }
-//
-//
-//    public double getX() {
-//        return x;
-//    }
-//
-//    public double getY() {
-//        return y;
-//    }
-//}
+
 class Reward implements renderable {
     private double x;
     private double y;
