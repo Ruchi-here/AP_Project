3673db8451c274ec401698b62e0e21bc1783bf19
Ruchi Kumari <122676573+Ruchi-here@users.noreply.github.com>
2026-03-23 01:36:37 +0530

    Update Pillar.java

--- StickHeroGame/src/main/java/com/example/stickherog/Pillar.java
+++ StickHeroGame/src/main/java/com/example/stickherog/Pillar.java
@@ -1,19 +1,16 @@
-//package com.example.stickherog;
-//
-//import javafx.scene.canvas.GraphicsContext;
-//import javafx.scene.paint.Color;
-//
-//public class Pillar extends Platform {
-//    public Pillar(double x, double y, double width, double height) {
-//        super(x, y, width, height);
-//    }
-//
-//    // Additional methods or customization specific to Pillar can be added here
-//
-//    @Override
-//    public void render(GraphicsContext gc) {
-//        // Customize rendering for Pillar if needed
-//        gc.setFill(Color.BROWN);
-//        gc.fillRect(getX(), getY(), getWidth(), getHeight());
-//    }
-//}
+package com.example.stickherog;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Pillar extends Platform {
+   public Pillar(double x, double y, double width, double height) {
+       super(x, y, width, height);
+   }
+
+   @Override
+   public void render(GraphicsContext gc) {
+       gc.setFill(Color.BROWN);
+       gc.fillRect(getX(), getY(), getWidth(), getHeight());
+   }
+}
