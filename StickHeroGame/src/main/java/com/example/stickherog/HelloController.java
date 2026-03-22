4a679b79985c240724e132d0ec55956b0e7bba98
Ruchi Kumari <122676573+Ruchi-here@users.noreply.github.com>
2026-03-23 01:36:14 +0530

    Update HelloController.java

--- StickHeroGame/src/main/java/com/example/stickherog/HelloController.java
+++ StickHeroGame/src/main/java/com/example/stickherog/HelloController.java
@@ -1,15 +1,12 @@
-// StartScreenController.java
-//package com.example.stickherog;
-//
-//import javafx.event.ActionEvent;
-//import javafx.fxml.FXML;
-//
-//public class HelloController{
-//
-//    @FXML
-//    private void startGame(ActionEvent event) {
-//        // Handle the start game action
-//        // You can transition to the main game scene here
-//        HelloApplication.startGame();
-//    }
-//}
+package com.example.stickherog;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class HelloController{
+
+   @FXML
+   private void startGame(ActionEvent event) {
+       HelloApplication.startGame();
+   }
+}
