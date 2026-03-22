dc5b9501ceb8dbfd83afcb6f9615bf20d5eb766e
Ruchi Kumari <122676573+Ruchi-here@users.noreply.github.com>
2026-03-23 01:39:50 +0530

    Update Stick.java

--- StickHeroGame/src/main/java/com/example/stickherog/Stick.java
+++ StickHeroGame/src/main/java/com/example/stickherog/Stick.java
@@ -10,8 +10,9 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+
 public class Stick {
-    private static final double MAX_LENGTH = 300; // Maximum length for the stick
+    private static final double MAX_LENGTH = 300;
     private static final double STICK_SPEED = 300.0;
     private static final double FALL_SPEED = 50.0;
 
@@ -41,20 +42,16 @@ public class Stick {
             length += STICK_SPEED * deltaTime;
         }
     }
-//    public void falldiagonally(double deltaTime){
-////        Group root = new Group();
-////        Scene scene = new Scene(root, 400, 400);
-//        Rectangle stick =new Rectangle(100,100,20,300);
-//        stick.setFill(Color.ORANGE);
-////        root.getChildren().add(stick);
-//        FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), stick);
-//        fadeOut.setFromValue(1.0);
-//        fadeOut.setToValue(0.0);
-//        SequentialTransition fade = new SequentialTransition(fadeOut);
-//        fade.play();
-//
-//
-//    }
+    
+   public void falldiagonally(double deltaTime){
+       Rectangle stick =new Rectangle(100,100,20,300);
+       stick.setFill(Color.ORANGE);
+       FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), stick);
+       fadeOut.setFromValue(1.0);
+       fadeOut.setToValue(0.0);
+       SequentialTransition fade = new SequentialTransition(fadeOut);
+       fade.play();
+   }
 
     public void stopExtension() {
         extending = false;
